05f0560c0d9ea9100c216b31acd3311454fb7f12
Unknown <ariasmaxi89@gmail.com>
2017-06-17 22:23:02 -0300

    angular lb

---
+++ client/js/controllers/compra.js
@@ -0,0 +1,41 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app')
+  .controller('ComprasController', ['$scope', '$state', 'Compra', function($scope,
+      $state, Compra) {
+    $scope.compras = [];
+    function getCompras() {
+      Compra
+        .find()
+        .$promise
+        .then(function(results) {
+          $scope.compras = results;
+        });
+    }
+    getCompras();
+
+    /*$scope.addCompra = function() {
+      Todo
+        .create($scope.newTodo)
+        .$promise
+        .then(function(todo) {
+          $scope.newTodo = '';
+          $scope.todoForm.content.$setPristine();
+          $('.focus').focus();
+          getTodos();
+        });
+    };
+
+    $scope.removeTodo = function(item) {
+      Todo
+        .deleteById(item)
+        .$promise
+        .then(function() {
+          getTodos();
+        });
+    };*/
+  }]);
